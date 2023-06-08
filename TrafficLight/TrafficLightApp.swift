ec52bbab714d161f064c00ba543299b393a3ccf5
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-06-08 21:26:11 +0400

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Iaroslav Beldin on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
